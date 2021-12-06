120135a7f99695d9ee4ac350198034c2445adf88
Goulloum <math.guillemin@hotmail.fr>
2021-12-06 01:26:34 +0100

    solution "provisoire" nav intra route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import HomePage from './Components/HomePage.js';
 import './App.css';
 
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import About from './Components/About';
 import Projects from './Components/Projects';
 import Skills from './Components/Skills';
@@ -12,26 +12,27 @@ import Contact from './Components/Contact';
 function App() {
   return (
     <Router>
-      <Switch>
-          <Route exact path="/">
-              <HomePage />
-          </Route>
-          <Route path="/About">
-              <About />
-          </Route>
-          <Route path="/Projects">
-              <Projects />
-          </Route>
-          <Route path="/Skills">
-              <Skills />
-          </Route>
-          <Route path="/Veille">
-              <Veille />
-          </Route>
-          <Route path="/Contact">
-              <Contact />
-          </Route>
-      </Switch>
+        {}
+        <Switch>
+            <Route exact path="/">
+                <HomePage />
+            </Route>
+            <Route path="/About">
+                <About />
+            </Route>
+            <Route path="/Projects">
+                <Projects />
+            </Route>
+            <Route path="/Skills">
+                <Skills />
+            </Route>
+            <Route path="/Veille">
+                <Veille />
+            </Route>
+            <Route path="/Contact">
+                <Contact />
+            </Route>
+        </Switch>
 
     </Router>
   );
