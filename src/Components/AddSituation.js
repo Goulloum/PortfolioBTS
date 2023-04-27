f3627b0345a64b9f69b18e5f11afa0822b51d149
mathieu.guillemin <mathieu.guillemin@iscio.com>
2023-04-27 18:20:10 +0200

    prod version

--- src/Components/AddSituation.js
+++ src/Components/AddSituation.js
@@ -1,51 +1,54 @@
-import axios from "axios"
+import axios from "axios";
 
-function AddSituation(){
+function AddSituation() {
+  const handleAddSituation = (e) => {
+    e.preventDefault();
+    console.log(e.target.img.files);
+    axios({
+      method: "post",
+      url: process.env.REACT_APP_API_URL + "/api/situation/addSituation",
+      data: {
+        name: e.target.name.value,
+        img: e.target.img.files,
+        description: e.target.description.value,
+        langages: e.target.langages.value,
+        date: e.target.date.value,
+        documents: e.target.documents.files,
+        productions: e.target.productions.files,
+        type: e.target.type.value,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((data) => {
+      console.log(data.data);
+    });
+  };
 
-    const handleAddSituation = (e) =>{
-        e.preventDefault()
-        console.log(e.target.img.files)
-        axios({
-            method: 'post',
-            url: 'http://localhost:8080/api/situation/addSituation',
-            data: {
-                name: e.target.name.value,
-                img: e.target.img.files,
-                description: e.target.description.value,
-                langages: e.target.langages.value,
-                date: e.target.date.value,
-                documents: e.target.documents.files,
-                productions: e.target.productions.files,
-                type: e.target.type.value
-            },
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }).then((data) => {
-            console.log(data.data)
-        })
-    } 
-
-    return(
-        <>
-            <div className="add-situation-title">Ajouter une situation :</div>
-            <form action="" onSubmit={(e) => handleAddSituation(e)}>
-                <input type="text" name="name" placeholder="nom" />
-                <label>Images</label>
-                <input type="file" multiple name="img" />
-                <textarea name="description" cols="30" rows="10" placeholder="description"></textarea>
-                <input type="text" name="langages" placeholder="langages" />
-                <input type="date" name="date" />
-                <label>Documents</label>
-                <input type="file" multiple name="documents"  />
-                <label>Productions</label>
-                <input type="file" multiple name="productions" />
-                <input type="text" name="type" placeholder="type" />
-                <input type="submit" value="Enregistrer" />
-            </form>
-        
-        </>
-    )
+  return (
+    <>
+      <div className="add-situation-title">Ajouter une situation :</div>
+      <form action="" onSubmit={(e) => handleAddSituation(e)}>
+        <input type="text" name="name" placeholder="nom" />
+        <label>Images</label>
+        <input type="file" multiple name="img" />
+        <textarea
+          name="description"
+          cols="30"
+          rows="10"
+          placeholder="description"
+        ></textarea>
+        <input type="text" name="langages" placeholder="langages" />
+        <input type="date" name="date" />
+        <label>Documents</label>
+        <input type="file" multiple name="documents" />
+        <label>Productions</label>
+        <input type="file" multiple name="productions" />
+        <input type="text" name="type" placeholder="type" />
+        <input type="submit" value="Enregistrer" />
+      </form>
+    </>
+  );
 }
 
-export default AddSituation
+export default AddSituation;
--- src/Components/BackOffice.js
+++ src/Components/BackOffice.js
@@ -1,59 +1,60 @@
-import {useState} from 'react'
-import axios from 'axios'
+import { useState } from "react";
+import axios from "axios";
 
-import AddSituation from './AddSituation';
+import AddSituation from "./AddSituation";
 
-function BackOffice(){
+function BackOffice() {
+  const [isLogged, setIsLogged] = useState(false);
+  const [error, setError] = useState(false);
 
-    const [isLogged, setIsLogged] = useState(false);
-    const [error, setError] = useState(false)
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios({
+      method: "post",
+      url: process.env.REACT_APP_API_URL + "/api/user/checkUser",
+      data: {
+        username: e.target.username.value,
+        password: e.target.password.value,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((data) => {
+      if (data.data) {
+        setIsLogged(true);
+      } else {
+        setError(true);
+        console.log(data.data);
+      }
+    });
+  };
 
-
-    const handleLogin = (e) => {
-        e.preventDefault()
-        axios({
-            method: 'post',
-            url: 'http://localhost:8080/api/user/checkUser',
-            data: {
-                username: e.target.username.value,
-                password: e.target.password.value
-            },
-            headers: {
-                "Content-Type": "application/json",
-            }
-        }).then((data) => {
-            if(data.data){
-                setIsLogged(true)
-            }else{
-                setError(true)
-                console.log(data.data)
-            }
-        })
-    }
-
-    if(!isLogged){
-        return(
-            <div className="backoffice-non-logged-container">
-
-                <form onSubmit={(e) => handleLogin(e)} action="">
-                    <input name='username' type="text" placeholder='Identifiant'/>
-                    <input name='password' type="password" placeholder='Mot de passe'/>
-                    <input type="submit" value="S'identifier"/>
-                </form>
-                {error? <div className='error-login-container'>Identifiant ou mot de passe incorrect</div>: ""}
-            </div>
-        )
-    }else{
-        return(
-            <div className="backoffice-container">
-                <div className="add-situation-container">
-                    <AddSituation />
-                </div>
-            </div>
-        )
-    }
-
-    
+  if (!isLogged) {
+    return (
+      <div className="backoffice-non-logged-container">
+        <form onSubmit={(e) => handleLogin(e)} action="">
+          <input name="username" type="text" placeholder="Identifiant" />
+          <input name="password" type="password" placeholder="Mot de passe" />
+          <input type="submit" value="S'identifier" />
+        </form>
+        {error ? (
+          <div className="error-login-container">
+            Identifiant ou mot de passe incorrect
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <div className="backoffice-container">
+        <div className="add-situation-container">
+          <AddSituation />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BackOffice;
